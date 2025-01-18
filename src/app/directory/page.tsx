4a693b27def184a0ba3db083f31ba40c446a1c28
Navoda Perera <navoda1@gmail.com>
2025-01-17 22:14:44 -0500

    Add facility Profile Page - Add facility profile page with the database ID as the route. - Update the directory items to act as links to the corresponding profile pages.

--- src/app/directory/page.tsx
+++ src/app/directory/page.tsx
@@ -24,7 +24,7 @@ export default async function DirectoryPage() {
     const facilities = await fetchData();
 
     return (
-        <div className="lg: flex min-w-[400px] flex-col gap-5 lg:grid lg:grid-cols-[400px,1fr] lg:items-start">
+        <div className="flex min-w-[400px] flex-col gap-5 lg:grid lg:grid-cols-[400px,1fr] lg:items-start">
             {/* Filter Card */}
             <Card className="h-auto">
                 <h1 className="pb-5 text-xl font-semibold">Filters</h1>
@@ -54,72 +54,77 @@ export default async function DirectoryPage() {
             <div className="flex min-w-[400px] flex-col gap-5 rounded-2xl">
                 {facilities.map((facility) => {
                     return (
-                        <Card key={facility.id}>
-                            <div className="flex gap-4">
-                                {facility.photos.length > 0 && (
-                                    <div className="relative block h-[180px] w-[180px] flex-shrink-0 overflow-hidden rounded-2xl bg-orange-50">
-                                        <Image
-                                            src="/images/facility-drone-image.jpg"
-                                            alt="facility-icon"
-                                            fill
-                                        />
-                                    </div>
-                                )}
-                                {facility.photos.length == 0 && (
-                                    <div className="relative block h-[180px] w-[180px] flex-shrink-0 overflow-hidden rounded-2xl bg-orange-50">
-                                        <Image
-                                            src="/images/facility-directory-icon-default.png"
-                                            alt="facility-icon"
-                                            fill
-                                            className="p-8"
-                                        />
-                                    </div>
-                                )}
-                                <div className="flex w-full flex-col gap-5">
-                                    {/* Title section */}
-                                    <div>
-                                        <div className="text-lg font-semibold">
-                                            {facility.name}
+                        <Link
+                            key={facility.id}
+                            href={`/facility/${facility.id}`}
+                        >
+                            <Card>
+                                <div className="flex gap-4">
+                                    {facility.photos.length > 0 && (
+                                        <div className="relative block h-[180px] w-[180px] flex-shrink-0 overflow-hidden rounded-2xl bg-orange-50">
+                                            <Image
+                                                src="/images/facility-drone-image.jpg"
+                                                alt="facility-icon"
+                                                fill
+                                            />
                                         </div>
-                                        <div className="text-sm">
-                                            {facility.type}
+                                    )}
+                                    {facility.photos.length == 0 && (
+                                        <div className="relative block h-[180px] w-[180px] flex-shrink-0 overflow-hidden rounded-2xl bg-orange-50">
+                                            <Image
+                                                src="/images/facility-directory-icon-default.png"
+                                                alt="facility-icon"
+                                                fill
+                                                className="p-8"
+                                            />
                                         </div>
-                                    </div>
+                                    )}
+                                    <div className="flex w-full flex-col gap-5">
+                                        {/* Title section */}
+                                        <div>
+                                            <div className="text-lg font-semibold">
+                                                {facility.name}
+                                            </div>
+                                            <div className="text-sm">
+                                                {facility.type}
+                                            </div>
+                                        </div>
 
-                                    {/* Address and contact section */}
-                                    <div className="flex flex-col gap-2">
-                                        <div className="flex items-center gap-2 text-sm">
-                                            <div>
-                                                <MapPin size={"1rem"} />
+                                        {/* Address and contact section */}
+                                        <div className="flex flex-col gap-2">
+                                            <div className="flex items-center gap-2 text-sm">
+                                                <div>
+                                                    <MapPin size={"1rem"} />
+                                                </div>
+                                                <div>
+                                                    {facility.location.address}
+                                                </div>
                                             </div>
-                                            <div>
-                                                {facility.location.address}
+                                            <div className="flex items-center gap-2 text-sm">
+                                                <div>
+                                                    <Phone size={"1rem"} />
+                                                </div>
+                                                <div>
+                                                    {facility.contact.phone.join(
+                                                        ", ",
+                                                    )}
+                                                </div>
                                             </div>
                                         </div>
-                                        <div className="flex items-center gap-2 text-sm">
-                                            <div>
-                                                <Phone size={"1rem"} />
-                                            </div>
-                                            <div>
-                                                {facility.contact.phone.join(
-                                                    ", ",
-                                                )}
-                                            </div>
-                                        </div>
-                                    </div>
 
-                                    {/* Region section */}
-                                    <div className="flex flex-col items-end">
-                                        <div className="flex items-center gap-2 text-sm">
-                                            {facility.location.district}
-                                        </div>
-                                        <div className="flex items-center gap-2 text-sm font-semibold">
-                                            {facility.location.province}
+                                        {/* Region section */}
+                                        <div className="flex flex-col items-end">
+                                            <div className="flex items-center gap-2 text-sm">
+                                                {facility.location.district}
+                                            </div>
+                                            <div className="flex items-center gap-2 text-sm font-semibold">
+                                                {facility.location.province}
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                        </Card>
+                            </Card>
+                        </Link>
                     );
                 })}
             </div>
---
+++ src/app/facility/[id]/page.tsx
@@ -0,0 +1,216 @@
+import { prisma } from "@/utils/db";
+import NotFound from "./NotFound";
+import Card from "@/components/Card";
+import Image from "next/image";
+import { MapPin, PhoneCall, Users } from "lucide-react";
+import Link from "next/link";
+import Button from "@/components/Button";
+
+type FacilityPageProps = {
+    params: {
+        id: string;
+    };
+};
+
+export default async function FacilityProfilePage({
+    params,
+}: FacilityPageProps) {
+    /* NextJS requirement: params should be awaited before accessing 
+        https://nextjs.org/docs/messages/sync-dynamic-apis*/
+    const { id } = await params;
+
+    const data = await prisma.childCareFacility.findUnique({
+        where: { id: id },
+    });
+
+    if (data == null) {
+        return NotFound();
+    }
+
+    /* Constants */
+    const noInfoText = "No Information";
+
+    /* Styles */
+    const infoSectionStyles =
+        "flex w-full xl:w-2/3 flex-col gap-2 divide-y divide";
+    const infoTableStyles = "py-4";
+    const infoTableHeaderStyles =
+        "flex items-center justify-start gap-2 pl-5 text-xl";
+    const infoTableRowStyles = "grid grid-cols-[3fr,4fr] items-center gap-4";
+
+    return (
+        <div className="m-5 flex justify-center">
+            <Card className="w-3/4 min-w-[600px]">
+                <div className="flex flex-col items-center justify-start gap-10 p-5">
+                    {/* Photo section */}
+                    <div className="relative block h-[180px] w-[180px] flex-shrink-0 overflow-hidden rounded-2xl bg-orange-50">
+                        <Image
+                            src="/images/facility-directory-icon-default.png"
+                            alt="facility-icon"
+                            fill
+                            className="p-8"
+                        />
+                    </div>
+
+                    {/* Info sections */}
+                    <div className="flex w-full flex-col items-center gap-14">
+                        {/* Title */}
+                        <div className="flex flex-col items-center">
+                            <div className="text-2xl font-bold">
+                                {data.name}
+                            </div>
+                            <div className="text-sm">{data.type}</div>
+                        </div>
+
+                        {/* Location */}
+                        <div className={infoSectionStyles}>
+                            {/* Header */}
+                            <div className={infoTableHeaderStyles}>
+                                <MapPin />
+                                <div className="font-semibold">Location</div>
+                            </div>
+
+                            {/* Table */}
+                            <div className={infoTableStyles}>
+                                <div className="flex flex-col gap-4 pl-10">
+                                    <div className={infoTableRowStyles}>
+                                        <div className="font-semibold">
+                                            Address
+                                        </div>
+                                        <div className="">
+                                            {data.location.address}
+                                        </div>
+                                    </div>
+                                    <div className={infoTableRowStyles}>
+                                        <div className="font-semibold">
+                                            Divisional Secretariat
+                                        </div>
+                                        <div className="">
+                                            {
+                                                data.location
+                                                    .divisionalSecretariat
+                                            }
+                                        </div>
+                                    </div>
+                                    <div className={infoTableRowStyles}>
+                                        <div className="font-semibold">
+                                            District
+                                        </div>
+                                        <div className="">
+                                            {data.location.district}
+                                        </div>
+                                    </div>
+                                    <div className={infoTableRowStyles}>
+                                        <div className="font-semibold">
+                                            Province
+                                        </div>
+                                        <div className="">
+                                            {data.location.province}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        {/* Contact */}
+                        <div className={infoSectionStyles}>
+                            {/* Header */}
+                            <div className={infoTableHeaderStyles}>
+                                <PhoneCall />
+                                <div className="font-semibold">Contact</div>
+                            </div>
+
+                            {/* Table */}
+                            <div className={infoTableStyles}>
+                                <div className="flex flex-col gap-4 pl-10">
+                                    <div className={infoTableRowStyles}>
+                                        <div className="font-semibold">
+                                            Phone
+                                        </div>
+                                        <div className="">
+                                            {data.contact.phone.length > 0
+                                                ? data.contact.phone.join(", ")
+                                                : noInfoText}
+                                        </div>
+                                    </div>
+                                    <div className={infoTableRowStyles}>
+                                        <div className="font-semibold">
+                                            Email
+                                        </div>
+                                        <div className="">
+                                            {data.contact.email.length > 0
+                                                ? data.contact.email
+                                                : noInfoText}
+                                        </div>
+                                    </div>
+                                    <div className={infoTableRowStyles}>
+                                        <div className="font-semibold">
+                                            District
+                                        </div>
+                                        <div className="">
+                                            {data.contact.website
+                                                ? data.contact.website
+                                                : noInfoText}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        {/* Residents */}
+                        <div className={infoSectionStyles}>
+                            {/* Header */}
+                            <div className={infoTableHeaderStyles}>
+                                <Users />
+                                <div className="font-semibold">Residents</div>
+                            </div>
+
+                            {/* Table */}
+                            <div className={infoTableStyles}>
+                                <div className="flex flex-col gap-4 pl-10">
+                                    <div className={infoTableRowStyles}>
+                                        <div className="font-semibold">
+                                            Total
+                                        </div>
+                                        <div className="">
+                                            {data.residents.total > 0
+                                                ? data.residents.total
+                                                : noInfoText}
+                                        </div>
+                                    </div>
+                                    <div className={infoTableRowStyles}>
+                                        <div className="font-semibold">
+                                            Male
+                                        </div>
+                                        <div className="">
+                                            {data.residents.male
+                                                ? data.residents.male
+                                                : noInfoText}
+                                        </div>
+                                    </div>
+                                    <div className={infoTableRowStyles}>
+                                        <div className="font-semibold">
+                                            Female
+                                        </div>
+                                        <div className="">
+                                            {data.residents.female
+                                                ? data.residents.female
+                                                : noInfoText}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* Buttons */}
+                    <div className="flex w-full justify-end">
+                        <Link href="/directory">
+                            <Button variant="secondary">Back</Button>
+                        </Link>
+                    </div>
+                </div>
+            </Card>
+        </div>
+    );
+}
