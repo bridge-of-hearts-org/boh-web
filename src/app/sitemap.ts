f095900b719dfdc4c466414afa7da748ad27dc32
Navoda Perera <navoda1@gmail.com>
2025-02-11 22:30:05 -0500

    Add robots.ts and sitemap.ts scripts to generate robots.txt and sitemap.xml files. All pages, including indivdual profile pages are added to the sitemap.

---
+++ src/app/sitemap.ts
@@ -0,0 +1,49 @@
+import type { MetadataRoute } from "next";
+import { fetchData } from "./directory/data";
+import { DirectoryFilterType } from "@/utils/defines";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+    const baseUrl = "https://bridgeofhearts.lk";
+
+    const filters: DirectoryFilterType = {
+        name: "",
+        city: "",
+        district: "",
+        province: "",
+    };
+
+    const [facilities, totalCount] = await fetchData(
+        filters,
+        "name",
+        1, // Page
+        1000, // Items per page (Get all the items)
+    );
+
+    if (totalCount > 1000) {
+        console.log(
+            "Warning: Not all facilities have been fetched for the sitemap generation",
+        );
+    }
+
+    /* Generate the sitemap for the facility pages */
+    const facilitySitemap = facilities.map((facility) => ({
+        url: `${baseUrl}/facility/${facility.id}`,
+        lastModified: new Date(),
+    }));
+
+    return [
+        {
+            url: baseUrl,
+            lastModified: new Date(),
+            changeFrequency: "monthly",
+            priority: 1,
+        },
+        {
+            url: `${baseUrl}/directory`,
+            lastModified: new Date(),
+            changeFrequency: "weekly",
+            priority: 0.8,
+        },
+        ...facilitySitemap,
+    ];
+}
