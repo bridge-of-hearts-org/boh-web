e7810973de2c224fe934da7821bcc6232a6a7fbc
Navoda Perera <navoda1@gmail.com>
2025-02-24 22:10:04 -0500

    Refactor the Footer section. - Add the Facebook logo with a link to the FB page. - Add the email address.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,8 @@
 import Link from "next/link";
 import ImageComponent from "./ImageComponent";
 
-import FooterLogo from "../../public/images/logo-dark-narrow.png";
+import BohLogo from "../../public/images/logo-dark-narrow.png";
+import FacebookLogo from "../../public/images/facebook-icon.svg";
 
 const footerLinkStyle = "font-normal text-gray-200";
 
@@ -10,7 +11,7 @@ export default function Footer() {
         <div className="flex w-screen bg-boh-black p-4 align-middle">
             <Link href="/">
                 <ImageComponent
-                    src={FooterLogo}
+                    src={BohLogo}
                     placeholder="blur"
                     width={100}
                     height={100}
@@ -19,7 +20,7 @@ export default function Footer() {
                     className="h-[100px] w-[100px] rounded-none object-contain p-2"
                 />
             </Link>
-            <div className="flex w-full flex-col items-center justify-between gap-10">
+            <div className="flex w-full flex-col items-center justify-between gap-8">
                 {/* <div className="flex w-full flex-col items-center justify-center gap-3 md:flex-row md:gap-7"> */}
                 <div className="flex w-full flex-wrap items-center justify-center gap-5 md:gap-10">
                     <Link href="/" className={footerLinkStyle}>
@@ -35,7 +36,7 @@ export default function Footer() {
                         Contact
                     </Link>
                 </div>
-                <div className="max-w-[900px] px-5 text-center text-[10px] text-white md:px-20">
+                <div className="max-w-[900px] px-5 text-center text-[10px] italic text-white md:px-20">
                     <p>
                         Some images displayed on this website are AI-generated.
                     </p>
@@ -48,8 +49,33 @@ export default function Footer() {
                         independently.
                     </p>
                 </div>
+                <div className="text-sm text-white lg:hidden">
+                    <a href="mailto:bridgeofheartslk@gmail.com">
+                        bridgeofheartslk@gmail.com
+                    </a>
+                </div>
             </div>
-            <div></div>
+            <div className="my-3 mr-4 flex flex-col items-end justify-between">
+                <a
+                    href="https://www.facebook.com/profile.php?id=61573039696750"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="boh-link"
+                >
+                    <ImageComponent
+                        src={FacebookLogo}
+                        width={40}
+                        sizes="40px"
+                        alt="Facebook Logo"
+                        className="invert"
+                    />
+                </a>
+                <div className="hidden text-sm text-white lg:block">
+                    <a href="mailto:bridgeofheartslk@gmail.com">
+                        bridgeofheartslk@gmail.com
+                    </a>
+                </div>
+            </div>
         </div>
     );
 }
