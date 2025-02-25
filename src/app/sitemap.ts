93cdeb07a797fe47b0a06cfab78e2da93fc4a3cf
Navoda Perera <navoda1@gmail.com>
2025-02-24 22:21:18 -0500

    Update the sitemap to include the About and Contact pages.

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -35,11 +35,23 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
         {
             url: baseUrl,
             lastModified: new Date(),
-            changeFrequency: "monthly",
+            changeFrequency: "weekly",
             priority: 1,
         },
         {
             url: `${baseUrl}/directory`,
+            lastModified: new Date(),
+            changeFrequency: "weekly",
+            priority: 0.9,
+        },
+        {
+            url: `${baseUrl}/about`,
+            lastModified: new Date(),
+            changeFrequency: "weekly",
+            priority: 0.8,
+        },
+        {
+            url: `${baseUrl}/contact`,
             lastModified: new Date(),
             changeFrequency: "weekly",
             priority: 0.8,
