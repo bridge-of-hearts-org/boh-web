0b4983f8d87a57cf93dc5a6474480773f411681a
Navoda Perera <navoda1@gmail.com>
2025-03-01 16:25:42 -0500

    Update the NavigationBar with a dropdown for page selection. - Remove individual buttons for each page. - Add Previous and Next buttons for pages. - Add a dropdown to select any arbitrary page.

--- src/app/directory/NavigationBar.tsx
+++ src/app/directory/NavigationBar.tsx
@@ -1,7 +1,10 @@
 "use client";
+import React from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 
 import Button from "@/components/Button";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+import Form from "next/form";
 
 type NavigationProps = {
     totalPages: number;
@@ -11,7 +14,7 @@ export default function NavigationBar(props: NavigationProps) {
     const router = useRouter();
     const searchParams = useSearchParams();
 
-    const currentPage = searchParams.get("page") || 1;
+    const currentPage: number = Number(searchParams.get("page")) || 1;
 
     const handleClick = (pageNumber: number) => {
         const params = new URLSearchParams(searchParams);
@@ -21,21 +24,54 @@ export default function NavigationBar(props: NavigationProps) {
     };
 
     return (
-        <div className="flex gap-5">
-            {props.totalPages > 1 &&
-                Array.from({ length: props.totalPages }, (_, i) => (
-                    <Button
-                        name={`Go to Page ${i + 1}`}
-                        key={i + 1}
-                        variant="secondary"
-                        color="black"
-                        disabled={currentPage == i + 1}
-                        className={`${currentPage == i + 1 ? "border-2 border-gray-400 bg-gray-50 shadow-none" : "bg-white shadow-md"}`}
-                        onClick={() => handleClick(i + 1)}
-                    >
-                        {i + 1}
-                    </Button>
-                ))}
+        <div className="flex items-center gap-9">
+            {currentPage > 1 && (
+                <Button
+                    name={`Go to the previous page`}
+                    variant="secondary"
+                    color="black"
+                    className="bg-white shadow-md"
+                    onClick={() => handleClick(currentPage - 1)}
+                >
+                    <ChevronLeft />
+                </Button>
+            )}
+            <div className="flex items-center gap-3">
+                <span>Page</span>
+                <Form action={() => {}}>
+                    <div className="flex items-center gap-5 text-sm">
+                        <select
+                            name="sortBy"
+                            value={currentPage}
+                            className="border-gray-200 bg-white"
+                            onChange={(e) => {
+                                handleClick(Number(e.target.value));
+                            }}
+                        >
+                            {Array.from(
+                                { length: props.totalPages },
+                                (_, i) => (
+                                    <option value={i + 1}>{i + 1}</option>
+                                ),
+                            )}
+                        </select>
+                    </div>
+                </Form>
+                <span>
+                    of <span className="font-bold">{props.totalPages}</span>
+                </span>
+            </div>
+            {currentPage < props.totalPages && (
+                <Button
+                    name={`Go to next page`}
+                    variant="secondary"
+                    color="black"
+                    className="bg-white shadow-md"
+                    onClick={() => handleClick(currentPage + 1)}
+                >
+                    <ChevronRight />
+                </Button>
+            )}
         </div>
     );
 }
