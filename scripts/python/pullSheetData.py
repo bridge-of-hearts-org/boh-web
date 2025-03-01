2ef49d6e50aa1f69999a1f22080118e12ceec2b1
Navoda Perera <navoda1@gmail.com>
2025-03-01 13:50:35 -0500

    Update the pullSheetData.py script - Add more verbose error reporting. - Add functionality to handle the Type and Sources fields. - Add an empty Genders field.

--- scripts/python/pullSheetData.py
+++ scripts/python/pullSheetData.py
@@ -44,7 +44,7 @@ def pullData(startCell: str, endCell: str, configFname: str = "config.json", api
         return df
 
 
-def downloadPhoto(url: str, outputDir: str) -> str:
+def downloadPhoto(facilityName: str, url: str, outputDir: str) -> str:
     try:
         resp = requests.get(url.strip())
         if resp.status_code == 200:
@@ -58,12 +58,13 @@ def downloadPhoto(url: str, outputDir: str) -> str:
                 with open(f"{outputDir}/{fileName}", "wb") as f:
                     f.write(resp.content)
                     return fileName
-
+            else:
+                print(f"Error: Could not determine file type: {facilityName} | {url}")
         else:
-            print(f"Error: Could not download photo: {url}")
+            print(f"Error: Could not download photo: {facilityName} | {url} | resp: {resp.status_code}")
 
     except Exception as e:
-        print(f"Error while downloading photo: {url}")
+        print(f"Error while downloading photo: {facilityName} | {url}")
         print(e.with_traceback())
 
     return ""
@@ -75,15 +76,16 @@ def generateJson():
     df = pd.read_csv(CSV_FNAME)
 
     # Define the columns to be selected
-    selectedColumns = ["Name", "Slug", "Address", "Managed By", "City", "District", "Province", "Divisional Secretariat", "Google Maps", "Phone Numbers", "Email Addresses", "Website",
+    selectedColumns = ["Name", "Slug", "Address", "Managed By", "Type", "City", "District", "Province", "Divisional Secretariat", "Google Maps", "Phone Numbers", "Email Addresses", "Website",
                        "Facebook", "Instagram", "Accepted Genders", "Age Range - Male", "Age Range - Female", "Age Range - All", "Count - Male", "Count - Female", "Count - Total",
-                       "Photos"]
+                       "Photos", "Sources"]
 
     # Rename columns
     df = df[selectedColumns].rename(columns={
         "Name": "name",
         "Slug": "slug",
         "Managed By": "managedBy",
+        "Type": "type",
         "Address": "location.address",
         "City": "location.city",
         "District": "location.district",
@@ -103,6 +105,7 @@ def generateJson():
         "Age Range - Female": "ageRanges.female",
         "Age Range - All": "ageRanges.all",
         "Photos": "photos",
+        "Sources": "sources"
     }).fillna("")
 
     # --- Clean up data ---
@@ -112,69 +115,81 @@ def generateJson():
 
     # Create lists from phone numbers
     df["contact.phone"] = df["contact.phone"].apply(
-        lambda listStr: [] if not listStr.strip() else [x.strip() for x in listStr.split(",")])
+        lambda listStr: [] if not listStr.strip() else [x.strip() for x in listStr.split(",")]
+    )
 
     # Create lists from email addresses
     df["contact.email"] = df["contact.email"].apply(
-        lambda listStr: [] if not listStr.strip() else [x.strip() for x in listStr.split(",")])
+        lambda listStr: [] if not listStr.strip() else [x.strip() for x in listStr.split(",")]
+        )
+
+    df["sources"] = df["sources"].apply(
+        lambda listStr: [] if not listStr.strip() else [x.strip() for x in listStr.split("|")]
+    )
 
     jsonData = []
 
     for idx, row in df.iterrows():
-        # --- Validate ---
-        assert row["genders"] in ["Male", "Female", "Both"]
-        assert np.all([number.startswith("0") and len(number) == 10
-                      for number in row["contact.phone"]])
-        assert np.all([email.__contains__("@")
-                      for email in row["contact.email"]])
+        # Only process if a slug is present
+        if row["slug"].strip():
+            # --- Validate ---
+            assert row["genders"].strip() in ["Male", "Female",
+                                              "Both", ""], f"Invalid gender: {row['genders']} | {row['name']}"
+            assert np.all([number.startswith("0") and len(number) == 10
+                           for number in row["contact.phone"]]), f"Invalid phone number: {row['contact.phone']} | {row['name']}"
+            assert np.all([email.__contains__("@")
+                           for email in row["contact.email"]])
 
-        # --- Construct the JSON structure ---
-        facility = {}
-        facility["name"] = row["name"].strip()
-        facility["slug"] = row["slug"].strip()
-        facility["type"] = "Voluntary Children's Home"
-        facility["managedBy"] = row["managedBy"].strip()
-        facility["location"] = {
-            "address": row["location.address"].strip(),
-            "city": row["location.city"].strip(),
-            "district": row["location.district"].strip(),
-            "province": row["location.province"].strip(),
-            "divisionalSecretariat": row["location.divisionalSecretariat"].strip(),
-            "google": row["location.google"].strip("/ ")
-        }
-        facility["contact"] = {
-            "phone": row["contact.phone"],
-            "email": row["contact.email"],
-            "website": row["contact.website"].strip("/ "),
-            "facebook": row["contact.facebook"].strip("/ "),
-            "instagram": row["contact.instagram"].strip("/ ")
-        }
-        facility["genders"] = row["genders"].strip().lower()
-        facility["occupancy"] = {
-            "total": int(row["occupancy.total"]) if row["occupancy.total"] else None,
-            "male": int(row["occupancy.male"]) if row["occupancy.male"] else None,
-            "female": int(row["occupancy.female"]) if row["occupancy.female"] else None
-        }
-        facility["ageRanges"] = {
-            "all": row["ageRanges.all"],
-            "male": row["ageRanges.male"],
-            "female": row["ageRanges.female"]
-        }
-        facility["photos"] = []
+            # --- Construct the JSON structure ---
+            facility = {}
+            facility["name"] = row["name"].strip()
+            facility["slug"] = row["slug"].strip()
+            facility["type"] = row["type"] if row["type"] else "Voluntary Children's Home"
+            facility["managedBy"] = row["managedBy"].strip()
+            facility["location"] = {
+                "address": row["location.address"].strip(),
+                "city": row["location.city"].strip(),
+                "district": row["location.district"].strip(),
+                "province": row["location.province"].strip(),
+                "divisionalSecretariat": row["location.divisionalSecretariat"].strip(),
+                "google": row["location.google"].strip("/ ")
+            }
+            facility["contact"] = {
+                "phone": row["contact.phone"],
+                "email": row["contact.email"],
+                "website": row["contact.website"].strip("/ "),
+                "facebook": row["contact.facebook"].strip("/ "),
+                "instagram": row["contact.instagram"].strip("/ ")
+            }
+            facility["genders"] = row["genders"].strip().lower() if row["genders"].strip() else "unknown"
+            facility["occupancy"] = {
+                "total": int(row["occupancy.total"]) if row["occupancy.total"] else None,
+                "male": int(row["occupancy.male"]) if row["occupancy.male"] else None,
+                "female": int(row["occupancy.female"]) if row["occupancy.female"] else None
+            }
+            facility["ageRanges"] = {
+                "all": row["ageRanges.all"],
+                "male": row["ageRanges.male"],
+                "female": row["ageRanges.female"]
+            }
+            facility["photos"] = []
+            facility["sources"] = row["sources"]
 
-        jsonData.append(facility)
-        if (row["photos"]):
-            outputDir = f"{PHOTOS_DIR}/{idx}"
-            os.makedirs(outputDir, exist_ok=True)
+            jsonData.append(facility)
+            if (row["photos"]):
+                outputDir = f"{PHOTOS_DIR}/{idx}"
+                os.makedirs(outputDir, exist_ok=True)
 
-            for url in row["photos"].split(","):
-                url = url.strip(" /")
+                for url in row["photos"].split("|"):
+                    url = url.strip(" /")
 
-                fname = downloadPhoto(url, outputDir)
-                facility["photos"].append({
-                    "fileName": fname,
-                    "source": url
-                })
+                    if url:
+                        fname = downloadPhoto(row["name"], url, outputDir)
+                        if fname:
+                            facility["photos"].append({
+                                "fileName": fname,
+                                "source": url
+                            })
 
     json.dump(jsonData, open(JSON_FNAME, "w"), indent=4,)
     print(f"Successfully generated the JSON file with {len(jsonData)} entries")
@@ -182,7 +197,7 @@ def generateJson():
 
 if __name__ == "__main__":
     endColumn = "AE"
-    endRow = 23
+    endRow = 94
 
     pullData("A1", f"{endColumn}{endRow}")
     generateJson()
